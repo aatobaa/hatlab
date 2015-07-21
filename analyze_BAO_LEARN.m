dff8477b8c01609ad90486165c8c8a0f8d2bd489
Adil Tobaa <aatobaa@uchicago.edu>
2015-07-21 12:48:01 -0500

    Full simulation tested and working. Two plots generated

--- analyze_BAO_LEARN.m
+++ analyze_BAO_LEARN.m
@@ -1,6 +1,14 @@
+%'results' is of the form:
+% {'filename' BAM BAT model R2 PValue VS WVS {nx8 cell}}
+%'{nx8 cell}' is of the form:
+% {'set of trials' BAM BAT model R2 PValue VS WVS}
+
 %List of Datasets:
 DATASETS = ['M1TM_20111014'; 'M1TM_20111017'; 'M1TM_20111019'; 'M1TM_20111021'; 'M1TM_20111025'];
 results = cell(size(DATASETS,1),9);
+
+%Eventually want to add P-Values for weighted and unweighted vector
+%strengths.
 
 %Indices for the cell of results.
 INDX_FILENAME = 1;
@@ -53,7 +61,7 @@ for d = 1:size(DATASETS,1)
     results{d,INDX_BAT} = beta_attn_times;
     
     %% Chan2RC preparation 1/2 (needs internet)
-    chan2rc = makechan2rc_mac('y','mio');
+%     chan2rc = makechan2rc_mac('y','mio');
     
     %% Chan2RC preparation 2/2 (doesn't need internet)
     plot_this_matrix = zeros(10,10);
@@ -87,9 +95,9 @@ for d = 1:size(DATASETS,1)
     results{d,INDX_R2} = R2;
     results{d,INDX_PValue} = pValue;    
     
-    %% Simulation: Simulate
+    %% Simulation: Simulate to compute Vector Strength of empirical 
     'Simulating'
-    NUM_SIMS = 100;
+    NUM_SIMS = 1000;
     BIN = 50;
     TRIAL_RANGE = 1:NUM_TRIALS;
     %Simulation actually calculates BAM; estimation is necessary with
@@ -214,7 +222,7 @@ for d = 1:size(DATASETS,1)
 
         %% Simulation: Simulate
         'Simulating'
-        NUM_SIMS = 100;
+        NUM_SIMS = 1000;
         BIN = 30;
         TRIAL_RANGE = binned_trials;
         %Simulation actually calculates BAM; estimation is necessary with
@@ -285,55 +293,156 @@ for d = 1:size(DATASETS,1)
 end
 %%
 
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ %%%%%                       PLOTS                      %%%%%%%%%%
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-
-
-
-
-
+%% CONSTANTS 
+NUM_ELEC = 96;
+NUM_OBS = 4001;
+%PEAK_FRQ Should be calculated again via power spectrum
+PEAK_FRQ = 18;
+BANDWIDTH = 3;
+SAMPLE_FRQ = 1000;
+%THRESHOLD for linear interpolation
+THRESHOLD = 0.15;
+%Segment of the data to use for computing BAT and BAM
+START_TIME = 500;
+END_TIME = 3500;   
 %%
-%currentBAT = BAT_M11014; %Use when you store Beta attenuation times so you
-%don't have to produce the same matrix over and over.
-%beta_attn_times = currentBAT;
 
 
-%% PLOT: Compute BAO and plot 10x10 square color-plot of BAT
-close all;
-hold on;
-axis off;
-axis square;
-axis([1 10 1 10]);
+%% Produce a BAT 10x10 plot for all datasets 
+for d = 1:size(DATASETS,1)
 
-% caxis([2100 2600]);
-for i=1:10
-    for j=1:10
-        c = plot_this_matrix(i,j);
-        if ~isnan(c) && c~=0
-            scatter(i,j,500,c,'filled');
-            elec_num = find(chan2rc(:,2)==i & chan2rc(:,1) == -(j-11));
-            %text(i,j,num2str(elec_num)); %Labels which electrode is each plotted circle
+    %% Preallocate
+    plot_this_matrix_BAM = zeros(10,10);
+    plot_this_matrix_BAT = zeros(10,10);
+
+    %% BAM Plot Prep
+    for i=1:128
+        if i <= NUM_ELEC
+            index = chan2rc(i,:);
+            plot_this_matrix_BAM(index(2),-index(1)+11) = results{d,INDX_BAM}(i);
         end
     end
+    %% BAT Plot Prep
+    for i=1:128
+        if i <= NUM_ELEC
+            index = chan2rc(i,:);
+            plot_this_matrix_BAT(index(2),-index(1)+11) = results{d,INDX_BAT}(i);
+        end
+    end
+    
+    %% Convert 0 to NaN for color plot 
+%     results{d,INDX_BAT}(~results{d,INDX_BAT}) = nan; %Turn the zeros into nans so they don't screw up the color plot
+%     results{d,INDX_BAM}(~results{d,INDX_BAM}) = nan;
+    %% Make plot
+    close all;
+    hold on;
+    axis off;
+    axis square;
+    axis([1 10 1 10]);
+
+    % caxis([2100 2600]);
+    for i=1:10
+        for j=1:10
+            c = plot_this_matrix_BAT(i,j);
+            if ~isnan(c) && c~=0
+                scatter(i,j,500,c,'filled');
+                elec_num = find(chan2rc(:,2)==i & chan2rc(:,1) == -(j-11));
+                %text(i,j,num2str(elec_num)); %Labels which electrode is each plotted circle
+            end
+        end
+    end
+    %% Compute BAO and place arrow
+    model = results{d, INDX_MODEL};
+    coefs = model.Coefficients.Estimate;
+    coefs_norm = norm([(coefs(3)/2),(coefs(2)/2)]);
+    R2 = results{d,INDX_R2};
+    %5.5 is the center of a plot with axis from 1 to 10; 10-1 = 9 / 2 = 4.5 + 1
+    % WHY IS THE Y-COORDINATE BEING SUBTRACTED HERE? 
+    arrow([5.5,5.5],[5.5+((coefs(3)/2)/coefs_norm)*(R2*4.5),5.5-((coefs(2)/2)/coefs_norm)*(R2*4.5)],'Width',3);
+
+    % set(get(colorbar),'Location','southoutside');\
+    c_bar = colorbar('southoutside');
+    set(get(c_bar, 'xlabel'), 'string', 'time (ms)')
+
+    saveas(gcf,strcat('BAT_PLOT_',results{d,INDX_FILENAME}),'epsc');
+
+    % NOTE: MODEL IS ONLY COMPUTED FOR BAT; NOT BAM. 
+     
 end
-coefs = eval(strcat(model,'.Coefficients.Estimate;'));
-coefs_norm = norm([(coefs(3)/2),(coefs(2)/2)]);
-%5.5 is the center of a plot with axis from 1 to 10; 10-1 = 9 / 2 = 4.5 + 1
-% WHY IS THE Y-COORDINATE BEING SUBTRACTED HERE? 
-arrow([5.5,5.5],[5.5+((coefs(3)/2)/coefs_norm)*(R2*4.5),5.5-((coefs(2)/2)/coefs_norm)*(R2*4.5)],'Width',3);
 
-if ~exist('BAO_coef','var')
-    BAO_coef = {coefs filename R2 'angle'};
-else
-    BAO_coef = [BAO_coef; {coefs filename R2 'angle'}];
+%% PLOT: BAO for each dataset. Compute Vector Strength. Run after computing BAO for all days
+[NUM_DATASETS,~] = size(results);
+ang=0:0.01:2*pi; 
+r = 1;
+xp=r*cos(ang);
+yp=r*sin(ang);
+plot(0+xp,0+yp, 'Color', 'black');
+axis([-1 1 -1 1]);
+axis square;
+axis off;
+angles = zeros(NUM_DATASETS,1);
+for d = 1:NUM_DATASETS
+    model = results{d, INDX_MODEL};
+    coefs = model.Coefficients.Estimate;
+    coefs_norm = norm([(coefs(3)/2),(coefs(2)/2)]);
+    t_filename = results{d, INDX_FILENAME};
+    
+    x = ((coefs(3)/2)/coefs_norm);
+    y = ((coefs(2)/2)/coefs_norm);
+    
+    r2 = results{d,INDX_R2};
+%     coefs = cell2mat(BAO_coef(i,1));
+%     t_filename = char(BAO_coef(i,2));
+%     coefs_norm = norm([(coefs(3)/2),(coefs(2)/2)]);
+    %index 3 is the R2 of the vector 
+%     r2 = cell2mat(BAO_coef(i,3));
+    %index 4 is the angle of the vector in radians
+    
+    angles(d) = acos(x);  
+    
+    
+    arrow([0,0],[0+x*r2,0-y*r2],'Width',1, 'Length', 3);
+    if(coefs(3)>0)
+        text(0+x*r2+0.02,0-y*r2, t_filename(length(t_filename)-2:length(t_filename)));
+    else
+        text(0+x*r2-.12,0-y*r2, t_filename(length(t_filename)-2:length(t_filename)));
+    end
+    
+    if d == NUM_DATASETS
+        vs = vectorStrength(angles);
+        wvs = weightedVectorStrength(angles,cell2mat(results(:,INDX_R2)));
+        
+        %% Simulation: Compute p-value for Vector Strength
+        %Generate random set of NUM_DATASETS vectors and R2 values [theta r2] rand from 0 to 2pi
+        NUM_SIMS_p = 100000;
+%         NUM_DATASETS = 20;
+        simulated_wvs = zeros(NUM_SIMS_p,1);
+        simulated_vs = zeros(NUM_SIMS_p,1);
+        for i = 1:NUM_SIMS_p
+            rand_vectors = [rand([NUM_DATASETS 1])*2*pi rand([NUM_DATASETS 1])];
+            sim_vs = vectorStrength(rand_vectors(:,1));
+            sim_wvs = weightedVectorStrength(rand_vectors(:,1),rand_vectors(:,2));
+            simulated_vs(i,:) = sim_vs;
+            simulated_wvs(i,:) = sim_wvs;
+        end
+
+        sum_vs = sum(simulated_vs >= vs);
+        sum_wvs = sum(simulated_wvs >= wvs);
+        p_valueVS = sum_vs / NUM_SIMS_p;
+        p_valueWVS = sum_wvs / NUM_SIMS_p;
+        %%
+        
+        text(0,-0.8,strcat('Vector Strength: ',num2str(vs), ' p = ', num2str(p_valueVS)), 'FontName', 'Arial','FontSize',12, 'FontWeight','bold');
+        text(0,-.9,strcat('Weighted (R2) Vector Strength: ',num2str(wvs), ' p = ', num2str(p_valueWVS)), 'FontName', 'Arial','FontSize',12, 'FontWeight','bold');
+    end
 end
 
-% set(get(colorbar),'Location','southoutside');\
-c_bar = colorbar('southoutside');
-set(get(c_bar, 'xlabel'), 'string', 'time (ms)')
-
-
-saveas(gcf,strcat('BAT_PLOT_',filename),'epsc');
 %% PLOT add-on: Plot all bootstrapped arrows
 for j = 1:NUM_SIMS
     plotX = ones(96,3);
@@ -344,15 +453,10 @@ for j = 1:NUM_SIMS
     arrow([5.5,5.5],[5.5+(coefs(3)/2)/coefs_norm*3,5.5-(coefs(2)/2)/coefs_norm*3],'Width',3);
 end
 % saveas(gcf,strcat('BAT_PLOT_BOOTSTRAP_',filename','eps')
-%%
-% BAT_M11025 = beta_attn_times;
-% BAT_M11025_boostrap = bootstrapped_BAM;
 
 %%
 %scatter(chan2rc(1:96,2), -chan2rc(1:96,1) + 11, 50 * ones(96,1), beta_attn_times(:), 'filled')
 %%The above line plots everything in the for loop in a better way.
-
-%Use ctrl+d to see what's under the hood of matlab
 
 %PLOTTING CRITERIA
 %Always label axes
@@ -365,8 +469,7 @@ end
 %[DONE] Replot maps so they all have same color scale
 %2 beta attenuations times for each electrode; plot first component on x
 %axis, second component on y axis
-%Our figures are on the bottoms; 6 total scatter plots.
-%IF NO ELECTRODE, DON'T PLOT IT. 
+
 %close all;
 %hold on;
 %axis on;
@@ -381,81 +484,14 @@ end
 
 %arrow([1500,1500],[1500+1*1000,1500+0.5733*1000],'Width',5,'Length',30);
 
-%%Try replotting the arrow for every X trials (to test learning
-%%hypothesis)
-%Where X is determined by how small you can get while still seeing the
-%sigmoidal shape in beta amplitude (so we can nicely filter where beta attn
-%"happens"
-
-%If we do need 50 trials, do a boostrap analysis, draw 1000 random samples
-%of 50 trials. (or draw 50 random trials, go through the whole motion)
 
 %To quantify circularity vs unimodality, use "vector strength"
-%Grab Matt's code or code yourself. Google it, one line mathematical
-%formula by Goldberg 1969ish. Or Matt/code folder
+%formula by Goldberg 1969ish. 
 %Look at the power spectrum across all electrodes to find the peak in beta
-%(make sure it's 18). Figure out how to compute a power spectrum. Vim's
+%(make sure it's 18). Vim's
 %book. 
 
-%% PLOT: BAO for each dataset. Compute Vector Strength. Run after computing BAO for all days
-%NOTE: Should we weight these by R2 value, when computing vector strength?
-[NUM_DATASETS,~] = size(BAO_coef);
-ang=0:0.01:2*pi; 
-r = 1;
-xp=r*cos(ang);
-yp=r*sin(ang);
-plot(0+xp,0+yp, 'Color', 'black');
-axis([-1 1 -1 1]);
-axis square;
-axis off;
-for i = 1:NUM_DATASETS
-    coefs = cell2mat(BAO_coef(i,1));
-    t_filename = char(BAO_coef(i,2));
-    coefs_norm = norm([(coefs(3)/2),(coefs(2)/2)]);
-    x = ((coefs(3)/2)/coefs_norm);
-    y = ((coefs(2)/2)/coefs_norm);
-    %index 3 is the R2 of the vector 
-    r2 = cell2mat(BAO_coef(i,3));
-    %index 4 is the angle of the vector in radians
-    BAO_coef(i,4) = {acos(x)};  
-    arrow([0,0],[0+x*r2,0-y*r2],'Width',1, 'Length', 3);
-    if(coefs(3)>0)
-        text(0+x*r2+0.02,0-y*r2, t_filename(length(t_filename)-2:length(t_filename)));
-    else
-        text(0+x*r2-.12,0-y*r2, t_filename(length(t_filename)-2:length(t_filename)));
-    end
-    
-    if i == NUM_DATASETS
-        vs = vectorStrength(cell2mat(BAO_coef(:,4)));
-        wvs = weightedVectorStrength(cell2mat(BAO_coef(:,4)),cell2mat(BAO_coef(:,3)));
-        text(0,-0.8,strcat('Vector Strength: ',num2str(vs)), 'FontName', 'Arial','FontSize',12, 'FontWeight','bold');
-        text(0,-.9,strcat('Weighted (R2) Vector Strength: ',num2str(wvs)), 'FontName', 'Arial','FontSize',12, 'FontWeight','bold');
-    end
-end
 
-%% Simulation: Compute p-value for Vector Strength
-%Generate random set of NUM_DATASETS vectors and R2 values [theta r2] rand from 0 to 2pi
-for j= 1:3
-    NUM_SIMS = 100000;
-    NUM_DATASETS = 20;
-    simulated_wvs = zeros(NUM_SIMS,1);
-    simulated_vs = zeros(NUM_SIMS,1);
-    for i = 1:NUM_SIMS
-        rand_vectors = [rand([NUM_DATASETS 1])*2*pi rand([NUM_DATASETS 1])];
-        sim_vs = vectorStrength(rand_vectors(:,1));
-        sim_wvs = weightedVectorStrength(rand_vectors(:,1),rand_vectors(:,2));
-        simulated_vs(i,:) = sim_vs;
-        simulated_wvs(i,:) = sim_wvs;
-    end
-
-    sum_vs = sum(simulated_vs >= vs);
-    sum_wvs = sum(simulated_wvs >= wvs);
-    p_valueVS = sum_vs / NUM_SIMS;
-    p_valueWVS = sum_wvs / NUM_SIMS;
-
-    [p_valueVS p_valueWVS]
-end
-%%
 
 
 
