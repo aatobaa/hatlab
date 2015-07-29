c007736213082c7551eb1cc0cc02fde3cfad6c21
Adil Tobaa <aatobaa@uchicago.edu>
2015-07-28 20:47:09 -0500

    fixed bounds error

--- build_data.m
+++ build_data.m
@@ -7,7 +7,8 @@ function all_data = build_data(filename,TOTAL_NUM_ELEC,NUM_OBS, PEAK_FRQ, BANDWI
     for i = 1:TOTAL_NUM_ELEC
         electrode_name = strcat('M1lfp_MB_elec',num2str(i,'%03d'));
         %if electrode exists
-        if strcmp(electrode_name,f(j).name)
+        num_working_elec = size(f,1);
+        if j <= num_working_elec && strcmp(electrode_name,f(j).name)
             load(filename, electrode_name);
             electrode_struct = arrayfun(@(x) x.times(:), eval(electrode_name), 'un', 0);
             all_data(:,~cellfun(@(x) all(isnan(x)), electrode_struct),i) = abs(hilbert(filterData(cell2mat(electrode_struct(~cellfun(@(x) all(isnan(x)), electrode_struct))),PEAK_FRQ,BANDWIDTH,SAMPLE_FRQ)));
